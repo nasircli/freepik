4bbf1db1b9a486bb9bfa2bd668f8c06851d5a0a0
Nasir Uddin <43319931+nasircli@users.noreply.github.com>
2023-11-24 13:57:51 +0600

    Create run_app.sh

---
+++ run_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source venv/bin/activate  # On Unix-like systems
+# venv\Scripts\activate  # On Windows
+
+# Run the Flask application with Gunicorn
+gunicorn -w 4 -b 0.0.0.0:5000 freepik:app
