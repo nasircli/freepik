01d4de0676d0e7465533efa1f043959c514b7aaf
Nasir Uddin <43319931+nasircli@users.noreply.github.com>
2023-11-24 14:43:43 +0600

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,14 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const crawlButton = document.getElementById('crawlButton');
+    crawlButton.addEventListener('click', submitForm);
+});
+
 function submitForm() {
     const mainInput = document.getElementById('mainInput').value;
+    // Perform any necessary validation on mainInput before making the request
 
-    // You can perform additional validation here
-
-    fetchCrawledData(mainInput);
-}
-
-function fetchCrawledData(mainInput) {
-    // You can use fetch API to make a request to your backend (Flask/Django) here
-    // and update the #outputContainer with the result.
-    // For simplicity, let's just display an alert for now.
-    alert(`Crawling data for: ${mainInput}`);
+    // Make an asynchronous request to your server with the mainInput value
+    fetch('/crawl', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ mainInput: mainInput }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Update the outputContainer with the response from the server
+        const outputContainer = document.getElementById('outputContainer');
+        outputContainer.innerHTML = ''; // Clear previous content
+        if (data.crawled_data) {
+            const tagsList = document.createElement('ul');
+            data.crawled_data.forEach(tag => {
+                const tagItem = document.createElement('li');
+                tagItem.textContent = tag;
+                tagsList.appendChild(tagItem);
+            });
+            outputContainer.appendChild(tagsList);
+        } else if (data.error_message) {
+            const errorMessage = document.createElement('p');
+            errorMessage.textContent = data.error_message;
+            outputContainer.appendChild(errorMessage);
+        } else {
+            const noDataMessage = document.createElement('p');
+            noDataMessage.textContent = 'No data available.';
+            outputContainer.appendChild(noDataMessage);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle the error appropriately, e.g., display an error message to the user
+    });
 }
