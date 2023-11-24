76e6e242a83aab158ab3b39f09b96b3a03cb2fc2
Nasir Uddin <43319931+nasircli@users.noreply.github.com>
2023-11-24 14:21:28 +0600

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -5,5 +5,5 @@ source venv/bin/activate  # On Unix-like systems
 # venv\Scripts\activate  # On Windows
 
 # Run the Flask application with Gunicorn
-gunicorn -b 0.0.0.0:8000 app.wsgi:app
+gunicorn -b 0.0.0.0:8000 app:app
 
